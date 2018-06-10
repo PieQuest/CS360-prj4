9319abc066339f7ec4cec61355bce73fce47d773
PieQuest <piequest@outlook.com>
2018-06-10 15:25:13 -0700

    added free() and print tests

--- oscourse/linux-stable/cs300/process_ancestors.c
+++ oscourse/linux-stable/cs300/process_ancestors.c
@@ -35,22 +35,22 @@ asmlinkage long sys_process_ancestors(struct process_info info_array[], long siz
 		return -EFAULT;
 	}
 	*/
-	
+	printk("Inilized.\n");
     for (task = current; task != &init_task; task = task->parent) {
 
         process.pid = (long) task->pid;
-        //printk("Process ID: %ld\n", process.pid);
+        /printk("Process ID: %ld\n", process.pid);
 
         process.state = task->state;
         //printk("Process State: %ld\n", process.state);
 
         //printk("Process UID: %ld\n", process.uid);
         //printk("Process Name: ");
-
+		printk("Pass1.\n");
         memset(process.name, '\0', sizeof(process.name));
         strcpy(process.name, task->comm);
 
-
+		printk("Pass2.\n");
         list_for_each(list, &current->children);
         {
             //task = list_entry(list, struct task_struct, sibling);
@@ -58,6 +58,7 @@ asmlinkage long sys_process_ancestors(struct process_info info_array[], long siz
             count_children++;
             //if (task==task_struct) break;
         }
+		printk("Pass3.\n");
         list_for_each(list, &current->sibling);
         {
             //task = list_entry(list, struct task_struct, sibling);
@@ -65,6 +66,7 @@ asmlinkage long sys_process_ancestors(struct process_info info_array[], long siz
             count_sibling++;
             //if (task==task_struct) break;
         }
+		printk("Pass4.\n");
         process.num_children = count_children;
         process.num_siblings = count_sibling;
         //printk("Process num_children: %ld\n", process.num_children);
@@ -75,11 +77,13 @@ asmlinkage long sys_process_ancestors(struct process_info info_array[], long siz
 			return -EFAULT;
 		}
 		*/
-		
+		printk("Pass5.\n");
         info_array[count] = process;
 		
+		free(process.name);
+		
         count++;
-
+		printk("Pass6.\n");
     }
 
     return 0;
