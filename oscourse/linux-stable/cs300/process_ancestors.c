f1ac00f96fee9e537cef222202a0e38431615d6d
PieQuest <piequest@outlook.com>
2018-06-10 19:19:05 -0700

    trying sid's code

--- oscourse/linux-stable/cs300/process_ancestors.c
+++ oscourse/linux-stable/cs300/process_ancestors.c
@@ -17,78 +17,51 @@
 
 //asmlinkage long sys_process_ancestors(struct process_info info_array[], long size, long *num_filled);
 
-asmlinkage long sys_process_ancestors(struct process_info info_array[], long size, long *num_filled) {
-    struct process_info process;
-    //struct task_struct task_struct;
-    struct task_struct *task;
-    struct list_head *list;
-
-    //long count=0;
-    long count_children = 0;
-    long count_sibling = 0;
-    long count = 0;
-    //int j=0;
-
-    if (size <= 0) return -EINVAL;
-    if (!info_array || !num_filled) return -EFAULT;
-	
-	/*
-	if (copy_from_user(info_array[i], process, sizeof(info_array[])))
-	{
-		return -EFAULT;
-	}
-	*/
-	printk("Inilized.\n");
-    for (task = current; task != &init_task; task = task->parent) {
-
-        process.pid = (long) task->pid;
-        //printk("Process ID: %ld\n", process.pid);
-
-        process.state = task->state;
-        //printk("Process State: %ld\n", process.state);
-
-        //printk("Process UID: %ld\n", process.uid);
-        //printk("Process Name: ");
-		printk("Pass1.\n");
-        memset(process.name, '\0', sizeof(process.name));
-        strcpy(process.name, task->comm);
-
-		printk("Pass2.\n");
-        list_for_each(list, &current->children);
-        {
-            //task = list_entry(list, struct task_struct, sibling);
-            //task now points to one of current’s children
-            count_children++;
-            //if (task==task_struct) break;
-        }
-		printk("Pass3.\n");
-        list_for_each(list, &current->sibling);
-        {
-            //task = list_entry(list, struct task_struct, sibling);
-            //task now points to one of current’s children
-            count_sibling++;
-            //if (task==task_struct) break;
-        }
-		printk("Pass4.\n");
-        process.num_children = count_children;
-        process.num_siblings = count_sibling;
-        //printk("Process num_children: %ld\n", process.num_children);
-
-		/*
-		if (copy_to_user(&info_array[count], &process, sizeof(process)))
-		{
-			return -EFAULT;
-		}
-		*/
-		printk("Pass5.\n");
-        info_array[count] = process;
-		
-		//free(process.name);
-		
-        count++;
-		printk("Pass6.\n");
+asmlinkage long sys_process_ancestors(struct process_info   info_array[],   long size, long *num_filled)
+{
+  struct task_struct* currentProcess = current;
+  struct process_info infoStruct;
+  struct list_head circLL_curr;
+  long structSize = sizeof(struct process_info);
+  long i;
+  long numChildren;
+  long numSiblings;
+  if(size <= 0) { return -EINVAL; }
+  for(i = 0; i < size; i++)
+  {
+    infoStruct.pid = currentProcess->pid;
+    strncpy(infoStruct.name, currentProcess->comm, ANCESTOR_NAME_LEN);
+    infoStruct.state = currentProcess->state;
+    infoStruct.uid = (long)currentProcess->cred->uid.val;
+    infoStruct.nvcsw = currentProcess->nvcsw;
+    infoStruct.nivcsw = currentProcess->nivcsw;
+    numChildren = 0;
+    numSiblings = 0;
+    circLL_curr = currentProcess->children;
+    while (circLL_curr.next!= &currentProcess->children)
+    {
+      numChildren++;
+      circLL_curr = *circLL_curr.next;
     }
-
-    return 0;
-
+    printk("number of children: %ld", infoStruct.num_children);
+    circLL_curr = currentProcess->sibling;
+    while (circLL_curr.next != &currentProcess->sibling)
+    {
+      numSiblings++;
+      circLL_curr = *circLL_curr.next;
+    }
+    printk("number of siblings: %ld", infoStruct.num_siblings);
+    infoStruct.num_children = numChildren;
+    infoStruct.num_siblings = numSiblings;
+    if( copy_to_user(&info_array[i], &infoStruct, structSize) )
+      return -EFAULT;
+    if(currentProcess == currentProcess->parent)
+    {
+      i++;
+      break;
+    }
+    currentProcess = (currentProcess->parent);
+  }
+  if( copy_to_user(num_filled, &i, sizeof(long)) ) { return -EFAULT; }
+  return 0;
 }
